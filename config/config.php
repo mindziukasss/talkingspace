4d77320d678ef2dc9ab79b7c86bda5538e9229bb
mindziukasss <mindziukasss@gmail.com>
2017-03-01 10:33:11 +0200

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define("DB_HOST", 'localhost');
+define("DB_USER", 'root');
+define("DB_PASS", '');
+define("DB_NAME", 'talkingspace');
+
+define('SITE_TITLE', 'Welcome to talking space!');
+
+define('BASE_URI', 'http://'.$_SERRVER['SERVER_NAME'].'/talkingspace/');
